b45bf8d729241ee63d84a2b95bb3c173de1088e0
Siva R <sivagrenganathan+github@protonmail.com>
2018-04-18 12:42:54 +0530

    Update models.py

--- excelplay_echo/core/models.py
+++ excelplay_echo/core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
-# userid will be changed to ONe to One to User auth object
+# userid will be changed to OneToOneField on integration with Auth.
 
 
-class Echouser(models.Model):
-    userid = models.IntegerField(primary_key=True, on_delete=models.CASCADE)
+class EchoUser(models.Model):
+    user_id = models.IntegerField(primary_key=True, on_delete=models.CASCADE)
     points = models.IntegerField()
     rank = models.IntegerField()
 
@@ -14,7 +14,7 @@ class Echouser(models.Model):
 
 
 class EchoUserSubmission(models.Model):
-    userid = models.ForeignKey(Echouser, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Echouser, on_delete=models.CASCADE)
     pid = models.ForeignKey(Problems, on_delete=models.CASCADE)
     files = models.FileField(null=True)
     sub_time = models.DateTimeField(auto_now_add=True, blank=True)
