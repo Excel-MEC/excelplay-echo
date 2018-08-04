4c3bf63ef87264cfa437d0ea0e41287d60245fea
kurianbenoy <kurian.pro@gmail.com>
2018-08-04 16:50:01 +0530

    Add judge for compiling

--- excelplay_echo/core/judge.py
+++ excelplay_echo/core/judge.py
@@ -1,2 +1,71 @@
+import os,subprocess,filecmp
+
 class Judge:
-    pass
+
+	def __init__(self,cwd):
+		self.cwd=cwd
+		# change directory
+		os.chdir(self.cwd)
+		self.timelimit = 1
+
+	def compile(self,fid):
+		"""
+		bash ./filename.sh
+		if possible change file permissions
+		add #!/bin/bash at beginning
+		AC = accept compilation
+		FC = failed compilation
+		"""
+		with open(self.cwd+"/tmp/err.txt",'w') as err:
+			cmd = cmd.replace("[filename]",fid)
+			t = subprocess.run(["chmod 700 (cmd).sh"],shell=True)
+			t.communicate()
+			response = t.returncode
+			t.kill()
+			if (response==0):
+				return "AC"
+			else:
+				return "FC"
+
+
+	def execute(self,fid):
+		"""
+		Functionaliy : execute shell script and
+		delivery and execute script
+		"""
+		
+		with open(self	.cwd + "/env/testcases/"+str(pid)+".txt","r") as input:
+			with open(self.cwd+"/tmp/temp.txt",'w') as output :
+				process=subprocess.Popen(['./(fid).sh'],preexec_fn=os.setsid,cwd=os.getcwd(),stdin=input,stdout=output)
+				try:
+					process.communicate(timeout=self.timelimit)
+				except subprocess.TimeoutExpired:
+					t = 125
+				if t==125:
+					return "ES"
+
+
+
+	def validate(self,fid):
+		#"test input and out"
+		if filecmp.cmp(self.cwd+"/tmp/output.txt",self.cwd+"/env/key/key"+str(pid)+".txt"==True):
+			return "AC"
+		else:
+			return "WA"
+
+def run(pid,filename):
+	cwd = os.getcwd()
+	fid = os.path.splitext(filename)[0]
+	ext = os.path.splitext(filename)[1]
+	obj = Judge(cwd)
+	ccf = obj.compile(pid,fid)
+	if ccf != "CS":
+		return ccf
+	ex = obj.execute(pid,fid)
+	if ex != "ES":
+		return ex
+	response = obj.validate(pid)
+	return(response)
+
+if __name__ == "__main__":
+	run()
