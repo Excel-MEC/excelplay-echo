562064f1919ce060b155a4521a19d8bb1352f385
kurianbenoy <kurian.pro@gmail.com>
2018-08-12 21:59:37 +0530

    Add view for submission

--- excelplay_echo/core/urls.py
+++ excelplay_echo/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from core.views import Echoleaderboard
+from core.views import Echoleaderboard,Submissionform
 
 urlpatterns =[
     path('leaderboard',Echoleaderboard.as_view()),
+    path('submit',Submissionform.as_view())
 ]
