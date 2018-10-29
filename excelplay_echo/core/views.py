8854720ec565fa6f03520d7681371704e5498e6d
kurianbenoy <kurian.pro@gmail.com>
2018-10-29 16:11:48 +0530

    complete handshake

--- excelplay_echo/core/views.py
+++ excelplay_echo/core/views.py
@@ -1,10 +1,11 @@
+import request
 from django.shortcuts import render
 from core.models import EchoUser,EchoUserSubmission
 from core.serializers import EchoUserSerializer,EchoUserSubmissionSerializer
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
-from redisleaderboard.wrapper import RedisLeaderboard
+from redis_leaderboard.wrapper import RedisLeaderboard
 
 rdb = RedisLeaderboard('redis',6380,0)
 
@@ -16,5 +17,7 @@ class Echoleaderboard(APIView):
 
 class Submissionform(generics.CreateAPIView):
     user = request.session['user']
+
+    euser = EchoUser.objects.get_or_create(user_id=user)
     queryset = EchoUserSubmission.objects.all()
     serializer_class = EchoUserSubmissionSerializer
