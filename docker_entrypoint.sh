0573e9dcc1420a7e9ed21d27db255d0cdbea0b49
kurianbenoy <kurian.pro@gmail.com>
2018-10-30 21:11:59 +0530

    Fix typo in docker entry

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python excelplay-echo/excelplay_echo/manage.py makemigrations && \
 	python excelplay-echo/excelplay_echo/manage.py migrate
-cd excelplay-dalalbull/excelplay_dalalbull
+cd excelplay-echo/excelplay_echo
 gunicorn excelplay_dalalbull.wsgi --bind 0.0.0.0:8003
