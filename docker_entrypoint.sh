415f571cfd33aa71e712650ac5f5c69ee8ec8b1e
Kurian Benoy <kurian.pro@gmail.com>
2018-10-31 16:30:45 +0530

    Update docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,4 +2,4 @@
 python excelplay-echo/excelplay_echo/manage.py makemigrations && \
 	python excelplay-echo/excelplay_echo/manage.py migrate
 cd excelplay-echo/excelplay_echo
-gunicorn excelplay_dalalbull.wsgi --bind 0.0.0.0:8003
+gunicorn excelplay_dalalbull.wsgi --bind 0.0.0.0:8004
